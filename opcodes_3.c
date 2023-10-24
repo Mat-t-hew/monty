16387299f32b6bc60d639914f91717752af03fb5
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2023-10-24 06:13:59 +0200

    Create opcodes_3.c

---
+++ opcodes_3.c
@@ -0,0 +1,56 @@
+#include "monty.h"
+
+/**
+ * mod - Modulo operation on the top two elements of the stack.
+ * @stack: Double pointer to the stack.
+ * @line_number: The line number in the bytecode file.
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
+/**
+ * pchar - Prints the character representation of the top stack element.
+ * @stack: Double pointer to the stack.
+ * @line_number: The line number in the bytecode file.
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+(void)line_number;
+}
+
+/**
+ * pstr - Prints the string starting at the top of the stack.
+ * @stack: Double pointer to the stack.
+ * @line_number: The line number in the bytecode file.
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+(void)line_number;
+}
+
+/**
+ * rotl - Rotates the stack to the top.
+ * @stack: Double pointer to the stack.
+ * @line_number: The line number in the bytecode file.
+ */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+(void)line_number;
+}
+
+/**
+ * rotr - Rotates the stack to the bottom.
+ * @stack: Double pointer to the stack.
+ * @line_number: The line number in the bytecode file.
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+(void)line_number;
+}
